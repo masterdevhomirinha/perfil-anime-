42f2845c313b65e0ab0162a2b7169f9ada5578a7
masterdevhomirinha <138265944+masterdevhomirinha@users.noreply.github.com>
2023-08-13 21:51:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-const generateButton = document.getElementById("generateButton");
-const animeImage = document.getElementById("animeImage");
-const downloadLink = document.getElementById("downloadLink");
+const animeImage = document.getElementById("anime-image");
+const generateButton = document.getElementById("generate-button");
+const genderSelect = document.getElementById("gender-select");
 
-generateButton.addEventListener("click", () => {
-    fetch("https://api.waifu.pics/sfw/neko")
-        .then(response => response.json())
-        .then(data => {
-            const imageUrl = data.url;
-            animeImage.src = imageUrl;
-            animeImage.style.display = "block";
-            downloadLink.href = imageUrl;
-            downloadLink.style.display = "block";
-        })
-        .catch(error => console.error("Erro ao carregar a imagem:", error));
-});
+const maleAnimeUrls = [
+    "https://example.com/male_anime_1.jpg",
+    "https://example.com/male_anime_2.jpg",
+    // Adicione mais URLs de imagens de anime masculino aqui
+];
+
+const femaleAnimeUrls = [
+    "https://example.com/female_anime_1.jpg",
+    "https://example.com/female_anime_2.jpg",
+    // Adicione mais URLs de imagens de anime feminino aqui
+];
+
+generateButton.addEventListener("click", generateImage);
+
+function generateImage() {
+    const selectedGender = genderSelect.value;
+
+    let animeUrls = [];
+    if (selectedGender === "male") {
+        animeUrls = maleAnimeUrls;
+    } else if (selectedGender === "female") {
+        animeUrls = femaleAnimeUrls;
+    } else {
+        // Se a opção "Aleatório" for selecionada, misture as URLs de meninos e meninas
+        animeUrls = maleAnimeUrls.concat(femaleAnimeUrls);
+    }
+
+    if (animeUrls.length > 0) {
+        const randomIndex = Math.floor(Math.random() * animeUrls.length);
+        const randomImage = animeUrls[randomIndex];
+        animeImage.src = randomImage;
+    }
+}
+
+// Chame generateImage() uma vez para mostrar uma imagem aleatória quando a página carregar
+generateImage();
